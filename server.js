cb9f2d15a96dce9fddeec216e03fa4e76d2c1655
adrianboon-cmd <sinhan95@gmail.com>
2026-09-15 15:21:04 +0800

    Update server.js

--- server.js
+++ server.js
@@ -106,7 +106,9 @@ async function getLineWorksAccessToken() {
   console.log("CLIENT_ID =", LINEWORKS_CLIENT_ID);
   console.log("SERVICE_ACCOUNT =", LINEWORKS_SERVICE_ACCOUNT);
   console.log("PRIVATE_KEY_EXISTS =", !!LINEWORKS_PRIVATE_KEY);
-
+console.log(
+  LINEWORKS_PRIVATE_KEY.substring(0, 50)
+);
   const now = Math.floor(Date.now() / 1000);
 
   if (
